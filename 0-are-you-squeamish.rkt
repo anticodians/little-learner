7c9c8e338714efa51de357287d62b6a8167108bb
michaelgfalk <michaelgfalk@gmail.com>
2025-02-26 14:32:37 +1100

    26 feb notes

--- 0-are-you-squeamish.rkt
+++ 0-are-you-squeamish.rkt
@@ -1,2 +1,27 @@
 #lang racket
 (define pie 3.14)
+
+(define a-radius 8.4)
+
+(define an-area
+  (* pie
+     (* a-radius a-radius)))
+
+(define compute-area-of-circle
+  (lambda (r)
+    (* pie
+       (* r r))))
+
+(define area-of-rectangle
+  (lambda (width)
+    (lambda (height)
+      (* width height))))
+
+(define double-result-of-f
+  (lambda (f)
+    (lambda (z)
+      (* 2 (f z)))))
+
+(define add3
+  (lambda (x)
+    (+ 3 x)))
